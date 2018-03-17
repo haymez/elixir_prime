516ceb7a6bd3387c039d208acb135a26d4c63472
James Miltenberger <jamilt13@gmail.com>
2018-03-17 11:00:19 -0500

    Module is working properly
    * Added docs and tests
    * Prime numbers are now found concurrently

--- lib/prime.ex
+++ lib/prime.ex
@@ -1,16 +1,32 @@
 defmodule Prime do
   @moduledoc """
-  Documentation for Prime.
+  Provides a set of algorithms to quickly find prime numbers given a range of
+  integers.
   """
+
+  @doc """
+  Splits `range` into sub lists to concurrently find all primes.
+  Order is not guaranteed.
+  """
+  @spec find_primes(list) :: list
+  def find_primes(range) do
+    range
+    |> Enum.to_list()
+    |> split_list(:erlang.system_info(:schedulers))
+    |> Enum.map(fn list -> Task.async(fn -> primes_for(list) end) end)
+    |> Enum.map(&Task.await(&1))
+    |> List.flatten()
+  end
 
   @doc """
   Filters `range` to only include prime numbers
 
   ## Examples
-  
+
       iex> Prime.primes_for(5..10)
       [5, 7]
   """
+  @spec primes_for(list) :: list
   def primes_for(range) do
     range
     |> Enum.filter(&is_prime?(&1))
@@ -19,19 +35,19 @@ defmodule Prime do
   def is_prime?(num) when num == 1, do: true
   def is_prime?(num) when num == 2, do: true
   def is_prime?(num) when rem(num, 2) == 0, do: false
-  
 
   @doc """
   Tests if `num` is a prime number.
 
   ## Examples
-  
+
       iex> Prime.is_prime?(5)
       true
 
       iex> Prime.is_prime?(9)
       false
   """
+  @spec is_prime?(integer) :: boolean
   def is_prime?(num) when is_integer(num) do
     not_prime =
       2..(num - 1)
@@ -41,7 +57,6 @@ defmodule Prime do
     !not_prime
   end
 
-  
   @doc """
   Tests if `potential_factor` is factor of `num`.
 
@@ -53,8 +68,22 @@ defmodule Prime do
       iex> Prime.is_factor?(21, 10)
       false
   """
-  # @spec is_factor?(number) :: integer
+  @spec is_factor?(integer, integer) :: boolean
   def is_factor?(num, potential_factor) do
     rem(num, potential_factor) == 0
+  end
+
+  @doc """
+  Splits `list` into `num` parts, as equally as possible.
+
+  ## Examples
+
+      iex> Prime.split_list([1,2,3,4], 2)
+      [[1, 2], [3, 4]]
+  """
+  @spec split_list(list, integer) :: list
+  def split_list(list, num) do
+    chunk_size = length(list) / num
+    list |> Enum.chunk_every(Float.ceil(chunk_size) |> Kernel.round())
   end
 end
--- test/prime_test.exs
+++ test/prime_test.exs
@@ -2,12 +2,12 @@ defmodule PrimeTest do
   use ExUnit.Case
   doctest Prime
 
-  test "Tests if factor" do
+  test "checks if factor" do
     assert Prime.is_factor?(10, 3) == false
     assert Prime.is_factor?(10, 5) == true
   end
 
-  test "Tests if number is prime" do
+  test "checks if number is prime" do
     assert Prime.is_prime?(1) == true
     assert Prime.is_prime?(2) == true
     assert Prime.is_prime?(3) == true
@@ -20,5 +20,54 @@ defmodule PrimeTest do
 
   test "finds all primes in list" do
     assert Prime.primes_for(1..5) == [1, 2, 3, 5]
+  end
+
+  test "finds all primes in list, concurrently" do
+    assert Prime.find_primes(2..100) == [
+             2,
+             3,
+             5,
+             7,
+             11,
+             13,
+             17,
+             19,
+             23,
+             29,
+             31,
+             37,
+             41,
+             43,
+             47,
+             53,
+             59,
+             61,
+             67,
+             71,
+             73,
+             79,
+             83,
+             89,
+             97
+           ]
+  end
+
+  test "Splits list as evenly as possible" do
+    assert Prime.split_list([1, 2, 3, 4, 5], 2) == [[1, 2, 3], [4, 5]]
+    assert Prime.split_list([1, 2, 3, 4, 5], 3) == [[1, 2], [3, 4], [5]]
+    assert Prime.split_list([1, 2, 3, 4, 5, 6, 7, 8], 3) == [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+    assert Prime.split_list(
+             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+             7
+           ) == [
+             [1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9],
+             [10, 11, 12],
+             [13, 14, 15],
+             [16, 17, 18],
+             [19]
+           ]
   end
 end
